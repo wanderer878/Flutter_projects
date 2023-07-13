7ff2f1793c1f6642f40200434e136db53359b495
wanderer878 <91022829+wanderer878@users.noreply.github.com>
2023-07-13 14:27:39 +0500

    laptop_commit

--- send_data_to_net/README.md
+++
@@ -1,3 +0,0 @@
-# send_data_to_net
-
-A new Flutter project.
--- send_data_to_net/android/app/src/main/kotlin/com/example/send_data_to_net/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.send_data_to_net
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
