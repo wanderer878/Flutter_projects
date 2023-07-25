5cb2174710d645403e249cfa2a73fa843cdf48bd
wanderer878 <usamaaqeel87@outlook.com>
2023-07-25 19:50:07 +0500

    Laptop_commit_websockets

--- Networking_cookbooks/flutter_websockets/lib/main.dart
+++ Networking_cookbooks/flutter_websockets/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:flutter/material.dart';
 
@@ -27,6 +29,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<String> msg_list = [];
+
   final TextEditingController _controller = TextEditingController();
   final _channel =
       WebSocketChannel.connect(Uri.parse("wss://echo.websocket.events"));
@@ -35,52 +38,62 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextFormField(
-            controller: _controller,
-            decoration: const InputDecoration(
-              labelText: 'Enter your message',
+          child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                labelText: 'Enter your message',
+              ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-            width: 20,
-            child: StreamBuilder(
-                stream: _channel.stream,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) ;
-                  msg_list.add(snapshot.data.toString());
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: StreamBuilder(
+                    stream: _channel.stream,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) ;
+                      msg_list.add(snapshot.data.toString());
+                      print(msg_list);
 
-                  return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: msg_list.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Text(msg_list[index]);
-                      });
-                }),
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              send_msg();
-            },
-            tooltip: 'send message',
-            child: const Icon(Icons.send),
-          )
-        ],
+                      return ListView.builder(
+                          itemCount: msg_list.length,
+                          itemBuilder: (context, index) {
+                            return Text(msg_list[index]);
+                          });
+                    }),
+              ),
+            ),
+          ],
+        ),
       )),
+      floatingActionButton: FloatingActionButton.extended(
+        isExtended: true,
+        onPressed: () {
+          send_msg();
+        },
+        label: Text('send message'),
+        icon: Icon(Icons.send),
+      ),
     );
   }
 
   void send_msg() {
     if (_controller.text.isNotEmpty) _channel.sink.add(_controller.text);
+    _controller.text = '';
   }
 
   @override
   void dispose() {
     _channel.sink.close();
     _controller.dispose();
+    msg_list = [];
     super.dispose();
   }
 }
