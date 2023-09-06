c640fcba167c67d85cefe580631fbb58b7df1727
wanderer878 <91022829+wanderer878@users.noreply.github.com>
2023-09-06 20:59:18 +0500

    Desktop_commit

--- testing/mockito_testing/test/main_test.dart
+++ testing/mockito_testing/test/main_test.dart
@@ -1,11 +1,37 @@
 //import 'dart:html';
 
 import 'package:http/http.dart' as http;
-import 'package:http/testing.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:mockito_testing/main.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'main_test.mocks.dart';
+import 'main_test.mocks.dart' as mocker;
+
+// bhai dart run build_runner build agr mocks.dart file create na krrha ho
+// build-runner package ko dev dependency ki jaga main dependency me rkh kr check krien.
 
 @GenerateMocks([http.Client])
-void main() {}
+void main() {
+  group("mockito http test", () {
+    test('successfull', () async {
+      final client = mocker.MockClient();
+
+      when(client
+              .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
+          .thenAnswer((_) async => http.Response(
+              '{"userId" : 1, "id" : 2, "title" : "hello_world"}', 200));
+
+      expect(await fetchAlbum(client), isA<Album>());
+    });
+
+    test("unsuccessful", () async {
+      final client = mocker.MockClient();
+
+      when(client
+              .get(Uri.parse("https://jsonplaceholder.typicode.com/albums/1")))
+          .thenAnswer((_) async => http.Response("Not Found", 404));
+
+      expect(fetchAlbum(client), throwsException);
+    });
+  });
+}
