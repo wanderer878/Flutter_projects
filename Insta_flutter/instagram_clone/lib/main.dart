65d885060b0128a89ccdd9c73dc6d2769b40ccfc
wanderer878 <91022829+wanderer878@users.noreply.github.com>
2023-12-03 18:48:58 +0500

    fyp commit

--- Insta_flutter/instagram_clone/lib/main.dart
+++ Insta_flutter/instagram_clone/lib/main.dart
@@ -8,7 +8,9 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android,
+  );
   runApp(const MainApp());
 }
 
