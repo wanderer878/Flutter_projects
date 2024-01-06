149bad1d6786e7fbf5383c0e487122245219f91b
wanderer878 <91022829+wanderer878@users.noreply.github.com>
2024-01-06 20:05:55 +0500

    fyp mobile screen

--- Insta_flutter/instagram_clone/lib/responsive/mobile_screen_layout.dart
+++ Insta_flutter/instagram_clone/lib/responsive/mobile_screen_layout.dart
@@ -1,13 +1,84 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
+
+  @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int _page = 0;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void On_page_changed(int current_page) {
+    setState(() {
+      _page = current_page;
+    });
+  }
+
+  void Navigation_tapped(int page) {
+    print(page);
+    pageController.jumpToPage(page);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("This is mobile"),
+      body: PageView(
+          onPageChanged: On_page_changed,
+          controller: pageController,
+          children: [
+            Center(
+              child: Text("This is home"),
+            ),
+            Center(
+              child: Text("This is search"),
+            ),
+            Center(
+              child: Text("This is addcircle"),
+            ),
+            Center(
+              child: Text("This is favourite"),
+            ),
+            Center(
+              child: Text("This is person"),
+            ),
+          ]),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home,
+                  color: _page == 0 ? primaryColor : secondaryColor)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search,
+                  color: _page == 1 ? primaryColor : secondaryColor)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle,
+                  color: _page == 2 ? primaryColor : secondaryColor)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite,
+                  color: _page == 3 ? primaryColor : secondaryColor)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person,
+                  color: _page == 4 ? primaryColor : secondaryColor)),
+        ],
+        onTap: Navigation_tapped,
       ),
     );
   }
