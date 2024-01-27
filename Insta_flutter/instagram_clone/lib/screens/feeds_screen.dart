ef01317c36cff22bebf9775198c293d74b8e76a3
wanderer878 <usamaaqeel87@outlook.com>
2024-01-27 21:04:51 +0500

    fyp post firebase

--- Insta_flutter/instagram_clone/lib/screens/feeds_screen.dart
+++ Insta_flutter/instagram_clone/lib/screens/feeds_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -15,22 +17,35 @@ class _FeedsState extends State<Feeds> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        title: SvgPicture.asset(
-          'assets/ic_instagram.svg',
-          height: 32,
-          color: primaryColor,
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          title: SvgPicture.asset(
+            'assets/ic_instagram.svg',
+            height: 32,
+            color: primaryColor,
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {}, icon: Icon(Icons.chat_bubble_outline_rounded))
+          ],
         ),
-        actions: [
-          IconButton(
-              onPressed: (){}, icon: Icon(Icons.chat_bubble_outline_rounded))
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 5.0),
-        child: PostCard(),
-      ),
-    );
+        body: StreamBuilder(
+            stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+            builder: (context,
+                AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: PostCard(snap: snapshot.data!.docs[index].data()),
+                    );
+                  });
+            }));
   }
 }
