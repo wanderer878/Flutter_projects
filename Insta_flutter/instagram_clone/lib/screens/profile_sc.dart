2d7d3ed39ff04a2e02b8c54214fb6b082f9de511
wanderer878 <91022829+wanderer878@users.noreply.github.com>
2024-03-19 14:49:31 +0500

    fyp profile

--- Insta_flutter/instagram_clone/lib/screens/profile_sc.dart
+++ Insta_flutter/instagram_clone/lib/screens/profile_sc.dart
@@ -18,7 +18,7 @@ class _Peofile_scState extends State<Profile_sc> {
         backgroundColor: mobileBackgroundColor,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             Row(
@@ -27,12 +27,40 @@ class _Peofile_scState extends State<Profile_sc> {
                   radius: 40,
                   backgroundImage: NetworkImage(
                       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      buildStatColumn(30, "posts"),
+                      buildStatColumn(160, "followers"),
+                      buildStatColumn(10, "following"),
+                    ],
+                  ),
                 )
               ],
             )
           ],
         ),
       ),
+    );
+  }
+
+  Column buildStatColumn(int num , String label){
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          num.toString(),
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          label,
+          style: TextStyle(fontSize: 10, color: Colors.grey, fontWeight: FontWeight.w600),
+        )
+      ],
     );
   }
 }
