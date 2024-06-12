4b4ffd4ba1011b5903c4349706220d61405cce1f
wanderer878 <91022829+wanderer878@users.noreply.github.com>
2024-06-12 16:06:45 +0500

    slivers codelab

--- flutter_practices/lib/sliver_list.dart
+++ flutter_practices/lib/sliver_list.dart
@@ -9,12 +9,29 @@ class Sliver_practice extends StatelessWidget {
         body: CustomScrollView(
       slivers: [
         SliverAppBar(
-          floating: true,
-          //pinned: true,
-          snap: true,
-          //expandedHeight: 200.0,
-
-          title: Text('Sliver appbar'),
+          pinned: true,
+          backgroundColor: Colors.purple[900],
+          centerTitle: true,
+          expandedHeight: 200.0,
+          flexibleSpace: FlexibleSpaceBar(
+            collapseMode: CollapseMode.parallax,
+            background: DecoratedBox(
+              position: DecorationPosition.foreground,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.center,
+                    colors: <Color>[
+                Colors.purple[900]!,
+                Colors.transparent
+              ])),
+              child:  Image.network(
+                  fit: BoxFit.cover,
+                  "https://images.unsplash.com/photo-1717400411765-0d6a4d0bc8db?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNDB8fHxlbnwwfHx8fHw%3D"),
+            ),
+            title: Text(style: TextStyle(color: Colors.white), "Sliver appbar"),
+            centerTitle: true,
+          ),
         ),
         List_generator()
       ],
