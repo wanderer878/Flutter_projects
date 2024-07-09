224b03291ced9fe56162c1237ba79390b36a2858
wanderer878 <91022829+wanderer878@users.noreply.github.com>
2024-07-09 14:28:22 +0500

    opencontainer

--- flutter_practices/lib/material_motion_animations/Details_screen.dart
+++ flutter_practices/lib/material_motion_animations/Details_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_practices/material_motion_animations/ProviderClass.dart';
+import 'package:provider/provider.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
+  
+  final int index;
+
+  const DetailsScreen({super.key, required this.index});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text(Provider.of<Providerclass>(context).games[index]),
+      ),
+    );
   }
 }
--- flutter_practices/lib/material_motion_animations/List_screen.dart
+++ flutter_practices/lib/material_motion_animations/List_screen.dart
@@ -1,5 +1,7 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_practices/material_motion_animations/Details_screen.dart';
 import 'package:flutter_practices/material_motion_animations/ProviderClass.dart';
 import 'package:provider/provider.dart';
 
@@ -21,13 +23,38 @@ class ListScreen extends StatelessWidget {
                 child: ListView.builder(
                     itemCount: games_list.length,
                     itemBuilder: (_, index) {
-                      return ListTile(
-                        title: Text(games_list[index]),
-                      );
+                      return TransformContainer(
+                          index: index,
+                          child: ListTile(
+                            title: Text(games_list[index]),
+                          ));
                     }),
               )
             ],
           ),
         ));
+  }
+}
+
+class TransformContainer extends StatelessWidget {
+  final int index;
+  final Widget child;
+  const TransformContainer(
+      {super.key, required this.index, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return OpenContainer(closedBuilder: (_, opencontainer) {
+      return InkWell(
+        onTap: () {
+          opencontainer();
+        },
+        child: child,
+      );
+    }, openBuilder: (_, closecontainer) {
+      return DetailsScreen(index: index);
+    },
+    openColor: Theme.of(context).cardColor,
+    );
   }
 }
