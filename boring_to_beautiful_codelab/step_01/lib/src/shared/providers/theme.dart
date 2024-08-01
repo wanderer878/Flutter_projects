ec216640e391a788ec723ea10d238cc6d1ec4c22
Usama Aqeel <usamaaqeel878@gmail.com>
2024-08-01 14:35:56 +0500

    boring to beautiful

--- boring_to_beautiful_codelab/step_01/lib/src/shared/providers/theme.dart
+++ boring_to_beautiful_codelab/step_01/lib/src/shared/providers/theme.dart
@@ -153,7 +153,7 @@ class ThemeProvider extends InheritedWidget {
   ThemeData light([Color? targetColor]) {
     final colorScheme = colors(Brightness.light, targetColor);
     return ThemeData.light(useMaterial3: true).copyWith(
-      // Add page transitions
+      pageTransitionsTheme: pageTransitionsTheme,
       colorScheme: colorScheme,
       appBarTheme: appBarTheme(colorScheme),
       cardTheme: cardTheme(),
@@ -170,7 +170,7 @@ class ThemeProvider extends InheritedWidget {
   ThemeData dark([Color? targetColor]) {
     final colorScheme = colors(Brightness.dark, targetColor);
     return ThemeData.dark(useMaterial3: true).copyWith(
-      // Add page transitions
+      pageTransitionsTheme: pageTransitionsTheme,
       colorScheme: colorScheme,
       appBarTheme: appBarTheme(colorScheme),
       cardTheme: cardTheme(),
--- boring_to_beautiful_codelab/step_01/lib/src/shared/views/outlined_card.dart
+++ boring_to_beautiful_codelab/step_01/lib/src/shared/views/outlined_card.dart
@@ -10,29 +10,62 @@ class OutlinedCard extends StatefulWidget {
     required this.child,
     this.clickable = true,
   });
-
   final Widget child;
   final bool clickable;
-
   @override
   State<OutlinedCard> createState() => _OutlinedCardState();
 }
 
 class _OutlinedCardState extends State<OutlinedCard> {
+  bool _hovered = false;
+
   @override
   Widget build(BuildContext context) {
+    final borderRadius = BorderRadius.circular(_hovered ? 20 : 8);
+    const animationCurve = Curves.easeInOut;
     return MouseRegion(
+      onEnter: (_) {
+        if (!widget.clickable) return;
+        setState(() {
+          _hovered = true;
+        });
+      },
+      onExit: (_) {
+        if (!widget.clickable) return;
+        setState(() {
+          _hovered = false;
+        });
+      },
       cursor: widget.clickable
           ? SystemMouseCursors.click
           : SystemMouseCursors.basic,
-      child: Container(
+      child: AnimatedContainer(
+        duration: kThemeAnimationDuration,
+        curve: animationCurve,
         decoration: BoxDecoration(
           border: Border.all(
             color: Theme.of(context).colorScheme.outline,
             width: 1,
           ),
+          borderRadius: borderRadius,
         ),
-        child: widget.child,
+        foregroundDecoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.onSurface.withOpacity(
+                _hovered ? 0.12 : 0,
+              ),
+          borderRadius: borderRadius,
+        ),
+        child: TweenAnimationBuilder<BorderRadius>(
+          duration: kThemeAnimationDuration,
+          curve: animationCurve,
+          tween: Tween(begin: BorderRadius.zero, end: borderRadius),
+          builder: (context, borderRadius, child) => ClipRRect(
+            clipBehavior: Clip.antiAlias,
+            borderRadius: borderRadius,
+            child: child,
+          ),
+          child: widget.child,
+        ),
       ),
     );
   }
