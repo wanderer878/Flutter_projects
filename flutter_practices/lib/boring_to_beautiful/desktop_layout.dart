79b44b1c96e90c3eb14f6cdd9ca1fad5313b19cd
Usama Aqeel <usamaaqeel878@gmail.com>
2024-08-05 15:38:06 +0500

    layout

---
+++ flutter_practices/lib/boring_to_beautiful/desktop_layout.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DesktopLayout extends StatelessWidget {
+  const DesktopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        //NavigationRail(destinations: destinations, selectedIndex: selectedIndex)
+      ],
+    );
+  }
+}
