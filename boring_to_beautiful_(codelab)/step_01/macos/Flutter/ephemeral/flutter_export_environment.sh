6e3318835ae650dd9f86d56bfb52930ae04833ec
wanderer878 <usamaaqeel87@outlook.com>
2024-08-11 15:11:19 +0500

    dell first commit

---
+++ boring_to_beautiful_(codelab)/step_01/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\Coding\flutter_windows_3.7.6-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\Coding\flutter_git\Flutter_projects\boring_to_beautiful_(codelab)\step_01"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1.0.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
