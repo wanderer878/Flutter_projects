65e63ea9b788c4fb2ef448383ac12b4db7f56d22
wanderer878 <usamaaqeel87@outlook.com>
2024-08-18 15:44:34 +0500

    MDC 102 (gridview)

--- flutter_practices/lib/MDC-102/cardgrid.dart
+++ flutter_practices/lib/MDC-102/cardgrid.dart
@@ -10,7 +10,17 @@ class CardGrid extends StatelessWidget {
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text(index.toString())],
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              child: Image.network(
+                "https://images.unsplash.com/photo-1721332154373-17e78d19b4a4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Text("data")
+          ],
         ),
       ),
     );
